b46c85e6a142e1ee426dab3506f73aac5d33512f
leeseungwon <free_sstyle@naver.com>
2022-05-10 13:06:27 +0900

    합과 정렬

--- main.cpp
+++ main.cpp
@@ -2,16 +2,60 @@
 
 using namespace std;
 
+void Bubble_Sort(int* Array, int Count)
+{
+	int Temp = 0;
+	for (int i = 0; i < Count; i++)
+	{
+		for (int j = 0; j < Count; j++)
+		{
+			if (j + 1 == Count - i)
+			{
+				break;
+			}
+			if (Array[j] > Array[j + 1])
+			{
+				Temp = Array[j];
+				Array[j] = Array[j + 1];
+				Array[j + 1] = Temp;
+			}
+		}
+	}
+}
 int main()
 {
-	int* Data = nullptr;
+	// 더하기
+	int Count = 0;
+	int Sum = 0;
+	int* Array = nullptr;
 
-	Data = new int;
+	cout << "입력 횟수 : ";
+	cin >> Count;
 
-	*Data = 100;
+	Array = new int[Count];
 
-	delete Data;
-	Data = nullptr;
+
+	for (int i = 0; i < Count; i++)
+	{
+
+		cout << "정수 : ";
+		cin >> Array[i];
+		Sum += Array[i];
+	}
+	cout << "합 : ";
+	cout << Sum << endl;
+
+	//정렬하기
+
+	Bubble_Sort(Array, Count);
+
+	cout << "정렬" << '\n';
+	for (int i = 0; i < Count; i++)
+	{
+		cout << Array[i] << endl;
+	}
+
+	delete[](Array);
 
 	return 0;
 }
