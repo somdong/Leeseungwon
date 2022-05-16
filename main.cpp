4a238959a0e22871a243211b180771b92b9b8214
leeseungwon <free_sstyle@naver.com>
2022-05-16 10:32:05 +0900

    //충돌체크 연습

--- main.cpp
+++ main.cpp
@@ -1,61 +1,129 @@
 #include <iostream>
+#include <conio.h>
+#include <windows.h>
 
 using namespace std;
 
-void Bubble_Sort(int* Array, int Count)
+struct FVector2D
 {
-	int Temp = 0;
-	for (int i = 0; i < Count; i++)
+	int X;
+	int Y;
+};
+
+char Map[10][10] = {
+	{'#','#','#','#','#','#','#','#','#','#'},
+	{'#',' ',' ',' ',' ',' ',' ',' ',' ','#'},
+	{'#',' ',' ',' ',' ',' ',' ',' ',' ','#'},
+	{'#',' ',' ',' ',' ',' ',' ',' ',' ','#'},
+	{'#',' ',' ',' ',' ','#',' ',' ',' ','#'},
+	{'#',' ',' ',' ',' ',' ',' ',' ',' ','#'},
+	{'#',' ',' ',' ','#',' ',' ',' ',' ','#'},
+	{'#',' ',' ',' ',' ',' ',' ',' ',' ','#'},
+	{'#',' ',' ',' ',' ',' ',' ',' ','G','#'},
+	{'#','#','#','#','#','#','#','#','#','#'}
+};
+
+
+bool IsCollide(const FVector2D& PredictPlayerPosition, FVector2D& OutPlayerPosition)
+{
+	if (Map[PredictPlayerPosition.Y][PredictPlayerPosition.X] == '#')
 	{
-		for (int j = 0; j < Count; j++)
-		{
-			if (j + 1 == Count - i)
-			{
-				break;
-			}
-			if (Array[j] > Array[j + 1])
-			{
-				Temp = Array[j];
-				Array[j] = Array[j + 1];
-				Array[j + 1] = Temp;
-			}
-		}
+		return false;
+	}
+	else
+	{
+		OutPlayerPosition = PredictPlayerPosition;
+		return true;
 	}
 }
+
+void SetLocation(FVector2D NewLocation);
+
+void DrawBG(FVector2D StartPosition)
+{
+	for (int Y = 0; Y < 10; ++Y)
+	{
+		for (int X = 0; X < 10; ++X)
+		{
+			FVector2D Temp;
+			Temp.X = X + StartPosition.X;
+			Temp.Y = Y + StartPosition.Y;
+			SetLocation(Temp);
+			cout << Map[Y][X];
+		}
+	}
+
+}
+
 int main()
 {
-	// 더하기
-	int Count = 0;
-	int Sum = 0;
-	int* Array = nullptr;
+	bool bRunning = true;
+	FVector2D PlayerPosition;
+	PlayerPosition.X = 1;
+	PlayerPosition.Y = 1;
 
-	cout << "입력 횟수 : ";
-	cin >> Count;
+	srand(static_cast<unsigned int>(time(nullptr)));
+	FVector2D StartPosition = { 0, 0 };
+	//StartPosition.X = rand() % 10 + 1;
+	//StartPosition.Y = rand() % 10 + 1;
 
-	Array = new int[Count];
-
-
-	for (int i = 0; i < Count; i++)
+	while (bRunning)
 	{
+		int KeyCode = _getch();
 
-		cout << "정수 : ";
-		cin >> Array[i];
-		Sum += Array[i];
+		FVector2D PredictPlayerPosition = PlayerPosition;
+
+		switch (KeyCode)
+		{
+		case 'w':
+		case 'W':
+			PredictPlayerPosition.Y--;
+			break;
+		case 's':
+		case 'S':
+			PredictPlayerPosition.Y++;
+			break;
+		case 'a':
+		case 'A':
+			PredictPlayerPosition.X--;
+			break;
+		case 'd':
+		case 'D':
+			PredictPlayerPosition.X++;
+			break;
+		case 27:
+			bRunning = false;
+			break;
+
+		}
+
+		IsCollide(PredictPlayerPosition, PlayerPosition);
+
+
+		//PlayerPosition.X = PlayerPosition.X < 1 ? 1 : PlayerPosition.X;
+		//PlayerPosition.Y = PlayerPosition.Y < 1 ? 1 : PlayerPosition.Y;
+		//PlayerPosition.X = PlayerPosition.X >= 9 ? 8 : PlayerPosition.X;
+		//PlayerPosition.Y = PlayerPosition.Y >= 9 ? 8 : PlayerPosition.Y;
+
+		system("cls");
+
+		DrawBG(StartPosition);
+		FVector2D Temp;
+		Temp.X = PlayerPosition.X + StartPosition.X;
+		Temp.Y = PlayerPosition.Y + StartPosition.Y;
+		SetLocation(Temp);
+		cout << "P";
 	}
-	cout << "합 : ";
-	cout << Sum << endl;
 
-	//정렬하기
-
-	Bubble_Sort(Array, Count);
-
-	cout << "정렬" << '\n';
-	for (int i = 0; i < Count; i++)
-	{
-		cout << Array[i] << endl;
-	}
-
-	delete[](Array);
 
 	return 0;
+}
+
+
+void SetLocation(FVector2D NewLocation)
+{
+	COORD Cur;
+	Cur.X = NewLocation.X;
+	Cur.Y = NewLocation.Y;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Cur);
 }
